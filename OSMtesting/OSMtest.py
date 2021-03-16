b55f307b336462b2060abe4043796ae900e262b0
epidermus <54918126+epidermus@users.noreply.github.com>
2021-03-15 18:24:57 -0600

    Git bash was much easier

---
+++ OSMtesting/OSMtest.py
@@ -0,0 +1,25 @@
+from OSMPythonTools.api import Api
+from OSMPythonTools.overpass import Overpass
+
+api = Api()
+overpass = Overpass()
+way = api.query('way/5887599')
+result = overpass.query('way["name"="Stephansdom"]; out body;')
+
+print(way.tag('building'))
+# 'castle'
+print(way.tag('architect'))
+# 'Johann Lucas von Hildebrandt'
+print(way.tag('website'))
+# 'http://www.belvedere.at'
+
+print("Overpass Test")
+
+stephansdom = result.elements()[0]
+
+print(stephansdom.tag('name:en'))
+
+result = overpass.query('node["amenity"="pub"](53.2987342,-6.3870259,53.4105416,-6.1148829); out;')
+
+pubs = result.elements()[0]
+print(pubs)
