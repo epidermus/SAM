cade47cef62b6635b983b72d06f387dfdbd91a00
Marcus <mbgoec2000@gmail.com>
2021-04-18 22:52:31 -0600

    Commented out test code so that it does not run when OSM stuff is imported

--- OSMtesting/OSMtest.py
+++ OSMtesting/OSMtest.py
@@ -38,21 +38,22 @@ count = 0
 for item in list:
     coords += str(item[1])[0:7] + "," + str(item[0])[0:7] + "/"
     count += 1
-print('SLC coords: ' + str(coords))
-print('Number of SLC coords loaded: ' + str(count) + '\n')
 
-image = ip.load_shape('apple-1.json')
-print('\nPoints of image before being converted to lat/long: ' + str(image))
-# testing the conversion of image points to lat long points
-# our ranges for Salt Lake... lat: 40.5928 - 40.7187 / long: -112.06 - -111.97
-# playing around with the ranges creates drastically different routes
-geo_image = ip.points_to_lat_long(image, 40.5928, 40.7, -112, -111.9)
-print('Points of image after being converted to lat/long: ' + str(geo_image))
-
-#construct coords to be placed into URL
-url_end = ''
-for point in geo_image:
-    url_end += str(point[0]) + ',' + str(point[1]) + '/'
-
-# showing route that will look like an apple in Google maps...
-webbrowser.open('https://www.google.com/maps/dir/' + url_end)
+#print('SLC coords: ' + str(coords))
+#print('Number of SLC coords loaded: ' + str(count) + '\n')
+#
+#image = ip.load_shape('apple-1.json')
+#print('\nPoints of image before being converted to lat/long: ' + str(image))
+## testing the conversion of image points to lat long points
+## our ranges for Salt Lake... lat: 40.5928 - 40.7187 / long: -112.06 - -111.97
+## playing around with the ranges creates drastically different routes
+#geo_image = ip.points_to_lat_long(image, 40.5928, 40.7, -112, -111.9)
+#print('Points of image after being converted to lat/long: ' + str(geo_image))
+#
+##construct coords to be placed into URL
+#url_end = ''
+#for point in geo_image:
+#    url_end += str(point[0]) + ',' + str(point[1]) + '/'
+#
+## showing route that will look like an apple in Google maps...
+#webbrowser.open('https://www.google.com/maps/dir/' + url_end)
