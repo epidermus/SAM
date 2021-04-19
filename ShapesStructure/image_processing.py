4e1bedba6a517a9dc0a71fd5ab579cc7e8b9e630
Marcus <mbgoec2000@gmail.com>
2021-04-18 21:31:52 -0600

    Added points_to_lat_long() function to convert image points to geographic coordinates

--- ShapesStructure/image_processing.py
+++ ShapesStructure/image_processing.py
@@ -4,41 +4,42 @@ import random
 import math
 import matplotlib.pyplot as plt
 from scipy.spatial.distance import directed_hausdorff
+from definitions import ROOT_DIR
 
 
 def load_shape(shape_file_name):
 	"""
-	loads in the point of a shape from the 2DShapesStructure data set
+	loads in the point of a shape from the ShapesStructure data set
 	:param shape_file_name: a string of the exact name of the JSON file located within shapes_JSON
 	:return: a list of tuples containing the x and y-coordinates of the shape
 	"""
-	with open('./Shapes_JSON/{}'.format(shape_file_name), 'r') as file:
+	with open(str(ROOT_DIR) + '\\ShapesStructure\\Shapes_JSON\\{}'.format(shape_file_name), 'r') as file:
 		data = json.load(file)
 		dict_points = data['points']
 		points = []
 		pointcount = 0
 		count = 0
 		for dict in dict_points:
-			pointcount +=1
-			if(pointcount == 4):
+			pointcount += 1
+			if (pointcount == 4):
 				points.append((dict['x'], dict['y']))
-				pointcount =0
-				count+=1
-		print(count)
+				pointcount = 0
+				count += 1
+		print('Number of points in loaded image: ' + str(count))
 		return points
 
 
 def load_random_shape():
 	"""
-	loads a random image from the 2DShapesStructure data set
+	loads a random image from the ShapesStructure data set
 	:return: a list of tuples containing the x and y-coordinates of the shapes
 	"""
-	path = './Shapes_JSON/'
+	path = str(ROOT_DIR) + '\\ShapesStructure\\Shapes_JSON\\'
 	random_filename = random.choice([
 		x for x in os.listdir(path)
 		if os.path.isfile(os.path.join(path, x))
 	])
-	print(random_filename)
+	print('Loaded image: ' + random_filename)
 	return load_shape(random_filename)
 
 
@@ -77,15 +78,6 @@ def scale_image(points, scale_factor):
 	for point in points:
 		scaled_x = scale_factor * point[0]
 		scaled_y = scale_factor * point[1]
-		# trying keep centroid of image in same position
-		#	if scale_factor < 1:
-		#		# image shrinks in this case
-		#		scaled_x += (point[0] - scaled_x) / 2
-		#		scaled_y += (point[1] - scaled_y) / 2
-		#	else:
-		# image grows in this case
-		#		scaled_x -= (scaled_x - point[0]) / 2
-		#		scaled_y -= (scaled_y - point[1]) / 2
 		scaled_points.append((scaled_x, scaled_y))
 	return scaled_points
 
@@ -99,6 +91,28 @@ def calculate_centroid(points):
 	x = [p[0] for p in points]
 	y = [p[1] for p in points]
 	return (sum(x) / len(points), sum(y) / len(points))
+
+
+# our ranges for Salt Lake are roughly... lat: 40.5928 - 40.7187 / long: -112.06 - -111.97
+def points_to_lat_long(points, lat_s, lat_f, long_s, long_f):
+	"""
+	converts a set of points (assuming a range of [0, 1] for x and y coordinates) to a set of lat and long coordinates
+	:param points: the points to be converted to geographical coordinates
+	:param long_s: the minimum longitude value in the plane
+	:param long_f: the maximum longitude value in the plane
+	:param lat_s: the minimum latitude value in the plane
+	:param lat_f: the maximum latitude value in the plane
+	:return: a list containing the new geographic coordinates of the points representing an image
+	"""
+	old_range = 1
+	new_range_long = long_f - long_s
+	new_range_lat = lat_f - lat_s
+	geo_points = []
+	for point in points:
+		lat = ((point[1] * new_range_lat) / old_range) + lat_s
+		long = ((point[0] * new_range_long) / old_range) + long_s
+		geo_points.append((lat, long))
+	return geo_points
 
 
 def draw_image(points, title=''):
@@ -118,27 +132,28 @@ def draw_image(points, title=''):
 	plt.plot(x_coords, y_coords)
 	plt.show()
 
-# testing points with directed_hausdorff from scipy
-frog = load_shape('frog-1.json')
-fork = load_shape('fork-1.json')
 
-# directed hausdorff from fork to frog
-directed_hdorff = directed_hausdorff(fork, frog)[0]
-print('Directed Hausdorff between frog and fork image: {}\n'.format(directed_hdorff))
-
-# symmetric hausdorff
-symmetric_hdorff = max(directed_hausdorff(frog, fork)[0], directed_hausdorff(fork, frog)[0])
-print('Symmetric Hausdorff between frog and fork image: {}\n'.format(symmetric_hdorff))
-
-# testing centroid
-print('Centroid of frog image: ' + str(calculate_centroid(frog)) + '\n')
-
-
-image = load_random_shape()
-
-# testing rotation and scaling functions
-draw_image(image, 'Random image before rotation')
-image = rotate_image(image, 90)
-draw_image(image, 'Random image rotated 90 degrees counterclockwise \n using image_processing.rotate_image function')
-image = scale_image(image, 2)
-draw_image(image, 'Random image rotated and scaled up by a factor of 2')
+## testing points with directed_hausdorff from scipy
+# frog = load_shape('frog-1.json')
+# fork = load_shape('fork-1.json')
+#
+## directed hausdorff from fork to frog
+# directed_hdorff = directed_hausdorff(fork, frog)[0]
+# print('Directed Hausdorff between frog and fork image: {}\n'.format(directed_hdorff))
+#
+## symmetric hausdorff
+# symmetric_hdorff = max(directed_hausdorff(frog, fork)[0], directed_hausdorff(fork, frog)[0])
+# print('Symmetric Hausdorff between frog and fork image: {}\n'.format(symmetric_hdorff))
+#
+## testing centroid
+# print('Centroid of frog image: ' + str(calculate_centroid(frog)) + '\n')
+#
+#
+# image = load_random_shape()
+#
+## testing rotation and scaling functions
+# draw_image(image, 'Random image before rotation')
+# image = rotate_image(image, 90)
+# draw_image(image, 'Random image rotated 90 degrees counterclockwise \n using image_processing.rotate_image function')
+# image = scale_image(image, 2)
+# draw_image(image, 'Random image rotated and scaled up by a factor of 2')
