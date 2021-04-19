4a3e7c4d6711ed7f67ea9b45904b17be5666b238
Marcus <mbgoec2000@gmail.com>
2021-04-18 23:22:29 -0600

    Stubbed out function that will be used to find more optimal routes for images

---
+++ route_optimizer.py
@@ -0,0 +1,39 @@
+from scipy.spatial.distance import directed_hausdorff
+import numpy as np
+
+
+def optimize_route(image, city_coords):
+	"""
+	utilizes scaling and rotation functions from image_processing.py to position an image from the 2DShapesStructure
+	dataset onto the roads of a city such that the roads closely resemble the original image. Hausdorff distance is used
+	as fitness metric of any single fitting.
+	:param image: the image to fit to a route
+	:param city_coords: the geographic coordinates of the streets of the city we are fitting an image to
+	:return:
+	"""
+	pass
+
+
+# A = np.array([(1.0, 0.0),
+#              (0.0, 1.0),
+#              (-1.0, 0.0),
+#              (0.0, -1.0)])
+
+# B = np.array([(2.0, 0.0),
+#              (0.0, 2.0),
+#              (-2.0, 0.0),
+#             (0.0, -4.0)])
+
+'''
+directed_hausdorff returns a 3-tuple where the first element is the directed hausdorff distance from A to B, and the
+second and third elements are the indexes of the points in A and B in their respective array representations that
+generated that distance
+'''
+# print(directed_hausdorff(A, B))
+
+'''
+note that scipy uses a "directed" hausdorff, meaning that, given sets of points A and B,
+directed_hausdorff(A, B) is not always equal to directed_hausdorff(B, A) (not symmetric). For a general (symmetric)
+Hausdorff distance, use the max() function:
+'''
+# print(max(directed_hausdorff(A, B)[0], directed_hausdorff(B, A)[0]))
